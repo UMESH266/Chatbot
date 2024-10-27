1f52de54b7bb5fe421fd6a317cc07808c185caf1
UMESH266 <144141198+UMESH266@users.noreply.github.com>
2024-10-27 18:17:25 +0530

    Chatbot streamlit version

--- avatar.py
+++ avatar.py
@@ -1,68 +1,139 @@
 # Main driver code of chatbot
+import streamlit as st
+from streamlit_option_menu import option_menu
 from src.components.avatarsys import AvatarSystem
 from src.utils.accessory import play_speech, listen, save_output, load_output
 import speech_recognition as sr
+
+# Global 
+salutation = "Pleasure meeting you. Have a nice day!"
 
 # Chatbot configuration initiation
 HF_TOKEN = "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"
 avatar = AvatarSystem(HF_TOKEN)
 
-# Chatting bot greetings
-welcome_text = "Hello, I am your chatbot assistant. How may I help you? \n Type 'Voice' or 'Text' to choose mode of interaction with me and say or type 'Exit' to end conversation."
-print(welcome_text)
-play_speech(welcome_text)
+# Page title
+st.title("Welcome to Chatbot conversation")
+st.markdown("<h3 style='text-align: center;'>Hello, I am your chatbot assistant.</h1>", unsafe_allow_html=True)
 
-# Mode of interaction selection 
-mode = input("Select mode of interaction (Voice/ Text/ Exit): ").lower()
+mode = option_menu("Choose mode of interaction", ["Text", "Voice", 'Exit'], 
+    icons=['house', 'cloud-upload', "list-task"], 
+    menu_icon="cast", default_index=0, orientation="horizontal")
 
-# while loop to have conversation with bot
-run = True
-while run:
-    user_input = '' # Initializing user input to be empty
-    if mode == "voice":
-        # Voice to text conversion
-        r = sr.Recognizer()
-        with sr.Microphone() as source:
-            print("Say something!")
-            audio = r.listen(source)
-            r.adjust_for_ambient_noise(source, duration=0.2)
-            text = r.recognize_google(audio)
-            user_input = text + '?'
-            
-            # Exiting the chat
-            if 'exit' in user_input:
-                print("Pleasure meeting you. Have a nice day!")
-                play_speech("Pleasure meeting you. Have a nice day!")
-                break
-
-    elif mode == "text":
-        while user_input != True:    
-            user_input = input("You: ").lower()
-            user_input = user_input + '?'
-
-            # Exiting the chat
-            if 'exit' in user_input:
-                print("Thank you, Bye!")
-                play_speech("Thank you, Bye!")
-                break
-
-    elif mode == 'exit':
-        print("Thank you, Bye!")
-        play_speech("Thank you, Bye!")
-        break
-    else:
-        print("Invalid mode. Try again")
-        print("Thank you, Bye!")
-        play_speech("Thank you, Bye!")
-        break    
+def response(input_text):
     # Getting response and sentiment of response 
-    output = avatar.process_input(user_input)
-
+    output = avatar.process_input(input_text)
     # Save output response in txt
     save_output(output)
     response_sentiment = output['emotion']
     ans = load_output()
-    print(f"You: {user_input}")
-    print(f"AI Avatar: {ans}\n")
+    st.write(f"You: {input_text}")
+    st.write(f"AI Avatar: {ans}\n")
     play_speech(ans)
-    print(f"Response sentiment: {response_sentiment}")
+    st.write(f"Response sentiment: {response_sentiment}")
+
+if mode == "Text":
+    # form requires unique key
+    with st.form(key='Chat form', clear_on_submit=True):
+        user_input = st.text_input("You: ", value="", placeholder="Ask anything or Type 'Exit' to end")
+        col1, col2, col3 = st.columns(3)
+        save = col3.form_submit_button("Submit")
+    if save and user_input != "":
+        user_input = user_input.lower() + '?'
+
+        # Exiting the chat
+        if 'exit' in user_input:
+            st.write(salutation)
+            play_speech(salutation)
+            st.stop()
+            
+        # Getting response and sentiment of response 
+        response(user_input)
+        
+elif mode == "Voice":
+    # Voice to text conversion
+    r = sr.Recognizer()
+    while 1:
+        with sr.Microphone() as source:
+            st.write("Speak: ") # print("Say something!")
+            st.write("Please wait, response under process...")
+            audio = r.listen(source)
+            r.adjust_for_ambient_noise(source, duration=0.2)
+            text = r.recognize_google(audio)
+            user_input = text + '?'
+            if text == '':
+                user_input='exit?'
+                st.write("Start again please. Failed to recognise the voice.")
+                
+            # Exiting the chat
+            if 'exit' in user_input:
+                st.write(salutation) # print("Pleasure meeting you. Have a nice day!")
+                play_speech(salutation)
+                st.stop()
+                break
+        
+            #Getting response and sentiment of response 
+            response(user_input)# output = avatar.process_input(user_input)
+        
+elif mode == "Exit":
+    st.write(salutation)
+    play_speech(salutation)
+    st.stop()
+
+
+def chatbot(mode):
+    # while loop to have conversation with bot
+    run = True
+    while run:
+        user_input = 'Hi!' # Initializing user input to be empty
+        if mode == "Voice":
+            # Voice to text conversion
+            r = sr.Recognizer()
+            with sr.Microphone() as source:
+                st.write("Speak: ") # print("Say something!")
+                audio = r.listen(source)
+                r.adjust_for_ambient_noise(source, duration=0.2)
+                text = r.recognize_google(audio)
+                user_input = text + '?'
+                
+                # Exiting the chat
+                if 'exit' in user_input:
+                    st.write(salutation) # print("Pleasure meeting you. Have a nice day!")
+                    play_speech(salutation)
+                    break
+
+        elif mode == "Text":
+            while user_input != True:    
+                user_input = st.text_input("You: ") # input("You: ").lower()
+                user_input = user_input.lower() + '?'
+
+                # Exiting the chat
+                if 'exit' in user_input:
+                    st.write(salutation)
+                    play_speech(salutation)
+                    break
+
+        elif mode == 'Exit':
+            st.write(salutation)
+            play_speech(salutation)
+            break
+        else:
+            st.write("Invalid mode. Try again")
+            st.write(salutation)
+            play_speech(salutation)
+            break    
+        
+        # Getting response and sentiment of response 
+        output = avatar.process_input(user_input)
+
+        # Save output response in txt
+        save_output(output)
+        response_sentiment = output['emotion']
+        ans = load_output()
+        st.write(f"You: {user_input}")
+        st.write(f"AI Avatar: {ans}\n")
+        play_speech(ans)
+        st.write(f"Response sentiment: {response_sentiment}")
+
+# Calling function
+# chatbot(mode)
